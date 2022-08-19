f3a26df0cade88339724a19e8f8a9055ec7347e1
fadhlialfarisi46 <fadhlialfarisi46@gmail.com>
2022-08-19 14:01:45 +0700

    initial commit

--- lib/presentation/pages/moviepages/home_movie_page.dart
+++ lib/presentation/pages/moviepages/home_movie_page.dart
@@ -1,14 +1,15 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ditonton/common/constants.dart';
+import 'package:ditonton/common/state_enum.dart';
 import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/presentation/pages/about_page.dart';
 import 'package:ditonton/presentation/pages/movie_detail_page.dart';
 import 'package:ditonton/presentation/pages/popular_movies_page.dart';
 import 'package:ditonton/presentation/pages/search_page.dart';
 import 'package:ditonton/presentation/pages/top_rated_movies_page.dart';
+import 'package:ditonton/presentation/pages/tvshow_page.dart';
 import 'package:ditonton/presentation/pages/watchlist_movies_page.dart';
 import 'package:ditonton/presentation/provider/movie_list_notifier.dart';
-import 'package:ditonton/common/state_enum.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -31,40 +32,7 @@ class _HomeMoviePageState extends State<HomeMoviePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/circle-g.png'),
-              ),
-              accountName: Text('Ditonton'),
-              accountEmail: Text('ditonton@dicoding.com'),
-            ),
-            ListTile(
-              leading: Icon(Icons.movie),
-              title: Text('Movies'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.save_alt),
-              title: Text('Watchlist'),
-              onTap: () {
-                Navigator.pushNamed(context, WatchlistMoviesPage.ROUTE_NAME);
-              },
-            ),
-            ListTile(
-              onTap: () {
-                Navigator.pushNamed(context, AboutPage.ROUTE_NAME);
-              },
-              leading: Icon(Icons.info_outline),
-              title: Text('About'),
-            ),
-          ],
-        ),
-      ),
+      drawer: NavigationDrawer(),
       appBar: AppBar(
         title: Text('Ditonton'),
         actions: [
@@ -202,3 +170,72 @@ class MovieList extends StatelessWidget {
     );
   }
 }
+
+class NavigationDrawer extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: AssetImage('assets/circle-g.png'),
+            ),
+            accountName: Text('Ditonton'),
+            accountEmail: Text('ditonton@dicoding.com'),
+          ),
+          ListTile(
+            leading: Icon(Icons.movie),
+            title: Text('Movies'),
+            onTap: () {
+              selectedItem(context, 0);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.tv),
+            title: Text('Tv Shows'),
+            onTap: () {
+              selectedItem(context, 1);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.save_alt),
+            title: Text('Watchlist'),
+            onTap: () {
+              selectedItem(context, 2);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.info_outline),
+            title: Text('About'),
+            onTap: () {
+              selectedItem(context, 3);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void selectedItem(BuildContext context, int index) {
+    if(index != 0){
+      Navigator.of(context).pop();
+    }
+
+    switch (index) {
+      case 0:
+        Navigator.pop(context);
+        break;
+      case 1:
+        Navigator.pushNamed(context, TvShowPage.ROUTE_NAME);
+        break;
+      case 2:
+        Navigator.pushNamed(context, WatchlistMoviesPage.ROUTE_NAME);
+        break;
+      case 3:
+        Navigator.pushNamed(context, AboutPage.ROUTE_NAME);
+        break;
+    }
+  }
+}
+
