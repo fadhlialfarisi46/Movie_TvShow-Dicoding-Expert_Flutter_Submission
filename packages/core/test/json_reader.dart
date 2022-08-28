b7f76a846b640914816dd1274b6d27a27de03120
fadhlialfarisi46 <fadhlialfarisi46@gmail.com>
2022-08-28 17:03:36 +0700

    move packages to 1 directory(packages) and implement caching data to local storage

--- packages/core/test/json_reader.dart
+++ packages/core/test/json_reader.dart
@@ -1,17 +1,15 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
-
 String readJson(String name) {
   var dir = Directory.current.path;
-  if (kDebugMode) {
-    print(dir);
-  }
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  // if (dir.endsWith('core')) {
-  //   return File('$dir/test/$name').readAsStringSync();
-  // }
-  return File('$dir/test/$name').readAsStringSync();
+  if (dir.endsWith('core')) {
+    return File('$dir/test/$name').readAsStringSync();
+  }
+  if (dir.endsWith('clone')) {
+    return File('$dir/packages/core/test/$name').readAsStringSync();
+  }
+  return File('$dir/core/test/$name').readAsStringSync();
 }
